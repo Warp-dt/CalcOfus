23c2cc5d9e5005f14cda5851ea8fe64f2a6992cc
Warp-dt <109367617+Warp-dt@users.noreply.github.com>
2025-04-14 22:12:39 +0000

    add full tacle

--- page_taclefuite.py
+++ page_taclefuite.py
@@ -44,35 +44,41 @@ p1=min(max((fuite+2)/(2*(tacles[0]+2)),0),1)
 p2=min(max((fuite+2)/(2*(tacles[1]+2)),0),1)
 p3=min(max((fuite+2)/(2*(tacles[2]+2)),0),1)
 ptot=p1*p2*p3
+couleur_table="#333B00"
+alerte_full_tacle=''
+if int(pm-(pm*ptot)//1)==pm:
+    couleur_table="#630000"
+    alerte_full_tacle=' **FULL TACLE !**'
+
 tab_res="""
 | PM perdus | PA perdus |
 | ----------- | ----------- |
 """
-tab_res+=f"""| {int(pm-(pm*ptot)//1)} | {int(pa-(pa*ptot)//1)} |"""
+tab_res+=f"""| {int(pm-(pm*ptot)//1)}{alerte_full_tacle} | {int(pa-(pa*ptot)//1)} |"""
 
 st.write(tab_res)
 
-table_style = """
+table_style = f"""
 <style>
-    table {
+    table {{
         width: 100%;  /* Le tableau prend toute la largeur disponible */
         border-collapse: collapse; /* Supprime les espaces entre les bordures */
         text-align: center; /* Centre le texte dans toutes les cellules */
 
-    }
-    th, td {
+    }}
+    th, td {{
         border: 1px solid white;  /* Bordures blanches autour des cellules */
         padding: 8px;  /* Ajoute un espace de 8px à l'intérieur des cellules */
-    }
-    th {
-        background-color: #333B00; /* Couleur d'arrière-plan des en-têtes */
+    }}
+    th {{
+        background-color: {couleur_table}; /* Couleur d'arrière-plan des en-têtes */
         color: #FAFAFA; /* Couleur du texte des en-têtes */
         
-    }
-    td {
+    }}
+    td {{
         background-color: #262730; /* Fond sombre des cellules */
         color: #FAFAFA; /* Texte blanc */
-    }
+    }}
 </style>
 """
 st.markdown(table_style, unsafe_allow_html=True)
